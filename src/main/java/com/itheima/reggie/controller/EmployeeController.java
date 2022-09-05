f1b99e6df9d84b5b155173ee20a8a94574e62ab4
Dora0111 <1679705493@qq.com>
2022-09-05 11:53:40 +0800

    修改提交

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -60,4 +60,18 @@ public class EmployeeController {
 
         return R.success(emp);
     }
+
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+
+        // 清理Session中保存的当前员工的id
+        request.getSession().removeAttribute("employee");
+
+        return R.success("退出成功");
+    }
 }
