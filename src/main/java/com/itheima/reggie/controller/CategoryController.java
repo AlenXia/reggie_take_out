907b563e1c9181c036f5de882fb9c9fdf9eb209b
Dora0111 <1679705493@qq.com>
2022-09-09 17:50:31 +0800

    day3修改

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -62,7 +62,9 @@ public class CategoryController {
     public R<String> delete(Long ids) {
         log.info("删除分类,id为：{}", ids);
 
-        categoryService.removeById(ids);
+//        categoryService.removeById(ids);
         return R.success("分类信息删除成功");
     }
+
+
 }
--- src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,52 @@
 package com.itheima.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.SetMeal;
 import com.itheima.reggie.mapper.CategoryMapper;
 import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.DishService;
+import com.itheima.reggie.service.SetMealService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+    @Autowired
+    private DishService dishService;
+
+    @Autowired
+    private SetMealService setMealService;
+
+    /**
+     * 根据id删除分类，删除之前需要进行判断
+     *
+     * @param id
+     */
+    @Override
+    public void remove(Long id) {
+        LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 添加查询条件，根据分类id进行查询
+        dishLambdaQueryWrapper.eq(Dish::getCategoryId, id);
+        int count = dishService.count(dishLambdaQueryWrapper);
+
+        // 查询当前分类是否关联了菜品，如果已经关联，则抛出一个业务异常
+        if (count > 0) {
+            // 已经关联了菜品，抛出一个业务异常
+        }
+
+        // 查询当前分类是否关联了套餐，如果已经关联，则抛出一个业务异常
+        LambdaQueryWrapper<SetMeal> setMealLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 添加查询条件，根据分类id进行查询
+        setMealLambdaQueryWrapper.eq(SetMeal::getCategoryId, id);
+        int count1 = setMealService.count();
+        if (count1 > 0) {
+            // 已经关联了套餐，抛出一个业务异常
+        }
+
+        // 正常删除分类
+        super.removeById(id);
+    }
 }
