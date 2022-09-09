10da079c4a4824497a41847b8fc241da6c15aad4
Dora0111 <1679705493@qq.com>
2022-09-09 16:50:18 +0800

    day3提交

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类管理
@@ -22,13 +21,48 @@ public class CategoryController {
 
     /**
      * 新增分类
+     *
      * @param category
      * @return
      */
     @PostMapping
-    public R<String> save(@RequestBody Category category){
-        log.info("category:{}",category);
+    public R<String> save(@RequestBody Category category) {
+        log.info("category:{}", category);
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        // 分页构造器
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        // 条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+
+        // 进行分页查询
+        categoryService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id来删除分类
+     *
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(Long ids) {
+        log.info("删除分类,id为：{}", ids);
+
+        categoryService.removeById(ids);
+        return R.success("分类信息删除成功");
     }
 }
