503ea634946e37b2856da25268b5046525b51533
Dora0111 <1679705493@qq.com>
2022-09-21 16:27:32 +0800

    day6修改

---
+++ src/main/java/com/itheima/reggie/service/impl/OrderServiceImpl.java
@@ -0,0 +1,49 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.BaseContext;
+import com.itheima.reggie.entity.Orders;
+import com.itheima.reggie.mapper.OrderMapper;
+import com.itheima.reggie.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implements OrderService {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AddressBookService addressBookService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    /**
+     * 用户下单
+     * @param orders
+     */
+    @Transactional
+    public void submit(Orders orders) {
+        //获得当前用户id
+        Long userId = BaseContext.getCurrentId();
+
+        // 查询当前用户的购物车数据
+
+
+        // 向订单表插入数据，一条数据 执行insert
+
+        // 向订单明细表插入数据，多条数据 执行insert
+
+        // 清空购物车数据
+
+
+    }
+}
